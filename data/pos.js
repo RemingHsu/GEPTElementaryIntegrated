089eed38693014bcc5676ef8a20365df93f97c5f
RemingHsu <reminghsu@gmail.com>
2026-05-24 17:21:47 +0800

    Add files via upload

---
+++ data/pos.js
@@ -0,0 +1,15 @@
+// GEPT Elementary 詞類縮寫對照
+window.POS_LEGEND = [
+  { abbr:"n.", full:"noun", zh:"名詞" },
+  { abbr:"v.", full:"verb", zh:"動詞" },
+  { abbr:"adj.", full:"adjective", zh:"形容詞" },
+  { abbr:"adv.", full:"adverb", zh:"副詞" },
+  { abbr:"pron.", full:"pronoun", zh:"代名詞" },
+  { abbr:"prep.", full:"preposition", zh:"介系詞" },
+  { abbr:"conj.", full:"conjunction", zh:"連接詞" },
+  { abbr:"aux.", full:"auxiliary verb", zh:"助動詞" },
+  { abbr:"art.", full:"article", zh:"冠詞" },
+  { abbr:"det.", full:"determiner", zh:"限定詞" },
+  { abbr:"int.", full:"interjection", zh:"感嘆詞" },
+  { abbr:"num.", full:"number", zh:"數詞" }
+];
